337400c26bc3862dd58d7a72955a6613afd5a3cc
carlos garcia <garcia1973@hotmail.com>
2026-03-15 22:16:13 -0400

    guardar photo_url en createClient

--- src/controllers/adminController.cjs
+++ src/controllers/adminController.cjs
@@ -1,84 +1,87 @@
 const { pool } = require("../config/db");
 const bcrypt = require("bcrypt");
 
-exports.createClient = async (req, res) => {
+    exports.createClient = async (req, res) => {
 
- try {
+    try {
 
-  const {
-   name,
-   last_name,
-   email,
-   phone,
-   gender,
-   birth_date,
-   emergency_contact_name,
-   emergency_contact_phone
-  } = req.body;
-
-  const password = "123456";
-
-  const hashedPassword = await bcrypt.hash(password,10);
-
-  const result = await pool.query(
-   `
-   INSERT INTO users
-   (
+    const {
     name,
     last_name,
     email,
-    password,
-    role,
     phone,
     gender,
     birth_date,
     emergency_contact_name,
     emergency_contact_phone,
-    created_at
-   )
-   VALUES
-   ($1,$2,$3,$4,'client',$5,$6,$7,$8,$9,NOW())
-   RETURNING id,name,email
-   `,
-   [
-    name,
-    last_name,
-    email,
-    hashedPassword,
-    phone,
-    gender,
-    birth_date,
-    emergency_contact_name,
-    emergency_contact_phone
-   ]
-  );
+    photo_url
+    } = req.body;
 
-  const user = result.rows[0];
+    const password = "123456";
 
-  const qrCode = `HIBRID-USER-${user.id}`;
+    const hashedPassword = await bcrypt.hash(password,10);
 
-  await pool.query(
-   `UPDATE users SET qr_code=$1 WHERE id=$2`,
-   [qrCode,user.id]
-  );
+    const result = await pool.query(
+    `
+    INSERT INTO users
+    (
+        name,
+        last_name,
+        email,
+        password,
+        role,
+        phone,
+        gender,
+        birth_date,
+        emergency_contact_name,
+        emergency_contact_phone,
+        photo_url,
+        created_at
+    )
+    VALUES
+    ($1,$2,$3,$4,'client',$5,$6,$7,$8,$9,$10,NOW())
+    RETURNING id,name,email
+    `,
+    [
+        name,
+        last_name,
+        email,
+        hashedPassword,
+        phone,
+        gender,
+        birth_date,
+        emergency_contact_name,
+        emergency_contact_phone,
+        photo_url
+    ]
+    );
 
-  res.json({
-   message:"Cliente creado",
-   user,
-   qr:qrCode
-  });
+    const user = result.rows[0];
 
- } catch(error){
+    const qrCode = `HIBRID-USER-${user.id}`;
 
-  console.error(error);
+    await pool.query(
+    `UPDATE users SET qr_code=$1 WHERE id=$2`,
+    [qrCode,user.id]
+    );
 
-  res.status(500).json({
-   error:"Error creando cliente"
-  });
+    res.json({
+    message:"Cliente creado",
+    user,
+    qr:qrCode
+    });
 
- }
+    } catch(error){
 
-};
+    console.error(error);
+
+    res.status(500).json({
+    error:"Error creando cliente"
+    });
+
+    }
+
+    };
 
 exports.getClients = async (req, res) => {
 
