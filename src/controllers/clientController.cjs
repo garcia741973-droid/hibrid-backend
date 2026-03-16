cea72448bc21861110cce887b42477832eab7acf
carlos garcia <garcia1973@hotmail.com>
2026-03-16 00:31:05 -0400

    mensaje

--- src/controllers/clientController.cjs
+++ src/controllers/clientController.cjs
@@ -86,4 +86,33 @@ exports.requestMembership = async (req, res) => {
     console.error(err);
     res.status(500).json({ error: "Error creando solicitud" });
   }
+
+    exports.getMe = async (req, res) => {
+
+    try {
+
+        const user_id = req.user.id;
+
+        const { rows } = await pool.query(
+        `SELECT
+            id,
+            name,
+            last_name,
+            qr_code,
+            membership_start,
+            membership_end
+        FROM users
+        WHERE id=$1`,
+        [user_id]
+        );
+
+        res.json(rows[0]);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Error obteniendo usuario" });
+    }
+
+    };
+
 };
--- src/routes/client.cjs
+++ src/routes/client.cjs
@@ -4,8 +4,10 @@ const router = express.Router();
 const controller = require('../controllers/clientController.cjs');
 const requireAuth = require('../middlewares/requireAuth.cjs');
 
+
 router.get('/plans', requireAuth, controller.getPlans);
 router.get('/payment-qr', requireAuth, controller.getPaymentQr);
 router.post('/request-membership', requireAuth, controller.requestMembership);
+router.get('/me', requireAuth, controller.getMe);
 
 module.exports = router;
