0517f6d1c06699c70d5291388fc8ffc9b96314f9
carlos garcia <garcia1973@hotmail.com>
2026-03-17 10:43:17 -0400

    mensaje

--- src/routes/cash.cjs
+++ src/routes/cash.cjs
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
-const { requireAuth } = require('../middlewares/auth');
+const pool = require('../db.js');
+const { requireAuth } = require('../middlewares/auth.cjs');
 
 
 // =========================
