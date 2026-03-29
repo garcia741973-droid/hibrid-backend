8d43a0fb28d27e6e95cee8278db981a478499aba
carlos garcia <garcia1973@hotmail.com>
2026-03-29 17:08:03 -0400

    mensaje

--- src/modules/trainer/trainerRoutes.cjs
+++ src/modules/trainer/trainerRoutes.cjs
@@ -4,7 +4,6 @@ const router = express.Router();
 const requireAuth = require('../../middlewares/requireAuth.cjs');
 const controller = require('./trainerController.cjs');
 
-const packagesController = require('./trainerPackagesController.cjs');
 
 router.post('/sessions', requireAuth, controller.createSession);
 router.get('/sessions', requireAuth, controller.getSessions);
@@ -13,8 +12,5 @@ router.put('/sessions/:id/status', requireAuth, controller.updateSessionStatus);
 router.post('/package-requests', requireAuth, controller.createPackageRequest);
 router.get('/package-requests', requireAuth, controller.getPackageRequests);
 router.post('/package-requests/:id/approve', requireAuth, controller.approveTrainerPackage);
-
-router.post('/packages', requireAuth, packagesController.createPackage);
-router.get('/packages', requireAuth, packagesController.getPackages);
 
 module.exports = router;
