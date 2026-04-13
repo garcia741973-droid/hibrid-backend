b467702e30a7260a059037c06066526d4a0f1f6b
carlos garcia <garcia1973@hotmail.com>
2026-04-12 20:27:42 -0400

    mensaje

--- src/server.js
+++ src/server.js
@@ -4,13 +4,18 @@ require("dotenv").config();
 
 const admin = require("firebase-admin");
 
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  })
+  credential: admin.credential.cert(serviceAccount),
 });
+
+console.log("🔥 FIREBASE PROJECT:", serviceAccount.projectId);
+console.log("🔥 FIREBASE CLIENT:", serviceAccount.clientEmail);
 
 const { pool } = require("./config/db");
 
