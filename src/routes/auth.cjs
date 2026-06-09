bf746a3e6a33418ed35d065ecb34b1d45d731c18
carlos garcia <garcia1973@hotmail.com>
2026-06-09 07:41:07 -0400

    mensaje

--- src/routes/auth.cjs
+++ src/routes/auth.cjs
@@ -4,6 +4,12 @@ const router = express.Router();
 const authController = require("../controllers/authController.cjs");
 const requireAuth = require("../middlewares/requireAuth.cjs");
 
+const {
+  sendResetCode,
+} = require(
+  '../services/emailService'
+);
+
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
@@ -14,5 +20,38 @@ router.get("/reminder", requireAuth, authController.getReminder);
 
 // 🔐 CAMBIAR PASSWORD (🔥 NUEVO)
 router.put("/change-password", requireAuth, authController.changePassword);
+
+router.get(
+  '/test-email',
+  async (req, res) => {
+
+    try {
+
+      await sendResetCode(
+
+        'garcia741973@gmail.com',
+
+        '123456',
+      );
+
+      res.json({
+        success: true,
+      });
+
+    } catch (e) {
+
+      console.log(
+        'EMAIL ERROR:',
+        e,
+      );
+
+      res.status(500).json({
+
+        error:
+          e.message,
+      });
+    }
+  }
+);
 
 module.exports = router;
